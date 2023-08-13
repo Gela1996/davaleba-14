e068e2c7ad0bac061fac632e58f1db21a6b6ed37
gela nikabadze <gela.nikabadze.1@gmail.com>
2023-08-13 20:56:00 +0400

    2 nawili

--- apps.js
+++ apps.js
@@ -1,30 +1,39 @@
 // დავალება 1
 
-let number = Number(prompt('Enter number'));
+// let number = Number(prompt('Enter number'));
 
-function isPrime(number) {
-if (number === 1 || number === 0) {
-   return false;
-} else {
-    for (let i = 2; i < number; i++) {
-        if (number % i === 0) {
-            return false;
-        }
-    }
-    return true;
+// function isPrime(number) {
+// if (number === 1 || number === 0) {
+//    return false;
+// } else {
+//   for (let i = 2; i < number; i++) {
+ //       if (number % i === 0) {
+ //           return false;
+  //      }
+//  }
+// return true;
+//}
+//}
+
+//alert(isPrime(number));
+
+//function showPrimes(number) {
+//   let result = ''
+//    for(let i = 2; i < number; i++) {
+//        if(isPrime(i)) {
+//            result += `${i}`;
+//        }
+//    }
+//    return result;
+// }
+
+//alert(showPrimes(number));
+
+// დავალება 2
+
+let table = document.body.firstElementChild;
+
+for (let i = 0; i < table.rows.length; i++) {
+  let row = table.rows[i];
+  row.cells[i].style.backgroundColor = 'red';
 }
-}
-
-alert(isPrime(number));
-
-function showPrimes(number) {
-    let result = ''
-    for(let i = 2; i < number; i++) {
-        if(isPrime(i)) {
-            result += `${i}`;
-        }
-    }
-    return result;
-}
-
-alert(showPrimes(number));
