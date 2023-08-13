02e3701eb1581039c2ff198400947f56965fe600
gela nikabadze <gela.nikabadze.1@gmail.com>
2023-08-13 20:58:35 +0400

    2 nawili

--- apps.js
+++ apps.js
@@ -31,9 +31,13 @@
 
 // დავალება 2
 
-let table = document.body.firstElementChild;
+ let table = document.body.firstElementChild;
 
-for (let i = 0; i < table.rows.length; i++) {
+ for (let i = 0; i < table.rows.length; i++) {
   let row = table.rows[i];
   row.cells[i].style.backgroundColor = 'red';
 }
+
+
+// დავალება 3
+
